8657e35b6d6c526ac62894192954849e3dc61af8
VD90 <vitek1149@gmail.com>
2021-02-23 19:39:37 +0300

    Сделал 3 задания, оставил комментарии в файлах

--- Dereglazov_Victor_dz_1/task_1_number_1.py
+++ Dereglazov_Victor_dz_1/task_1_number_1.py
@@ -1,18 +1,18 @@
 # untill seconds
-duration = int(53)
+duration = int(input("Введите время в секундах: "))
 s = duration % 60
 print("duration =", duration, "секунд")
 print(s, "сек")
 
 # untill minutes
-duration = int(153)
+duration = int(input("Введите время в секундах: "))
 m = duration // 60
 s = duration % 60
 print("duration =", duration, "секунд")
 print(m, "мин", s, "сек")
 
 # untill hours
-duration = int(4153)
+duration = int(input("Введите время в секундах: "))
 h = duration // 60 // 60
 m = duration // 60 % 60
 s = duration % 60
@@ -20,7 +20,7 @@ print("duration =", duration, "секунд")
 print(h, "час", m, "мин", s, "сек")
 
 # *untill 24 hours
-duration = int(5000000)
+duration = int(input("Введите время в секундах: "))
 d = duration // 60 // 60 // 24
 h = duration // 60 // 60 % 24
 m = duration // 60 % 60
